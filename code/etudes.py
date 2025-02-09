e9855ff889826a7a7faec27a99a783d0b6943c79
Emilia Kardache <emiliakardache@yahoo.com>
2025-02-09 21:37:19 +0100

    Ajout des images

--- code/etudes.py
+++ code/etudes.py
@@ -5,12 +5,14 @@ def afficher_page_etudes():
     st.title("Nos Études 📚")  
     st.write("Bienvenue sur la page dédiée à nos études. Voici quelques informations utiles.")
     
-   images = [
-    ("ETUDE ALEX ......", "static/images/image1.png"),
-    ("ETUDE ERIC ......", "static/images/image2.png"),
-    ("ETUDE EMILIA .....", "static/images/image3.png"),
-    ("ETUDE EMILIA .....", "static/images/image4.png"),
-]
+    images = [
+        ("ETUDE ALEX ......", "static/images/image1.png"),
+        ("ETUDE ERIC ......", "static/images/image2.png"),
+        ("ETUDE EMILIA .....", "static/images/image3.png"),
+        ("ETUDE EMILIA .....", "static/images/image4.png"),
+    ] 
+
+    
     for description, image in images:
         st.write(f"**{description}**")  # Texte en gras
         st.image(image, use_container_width=True)
