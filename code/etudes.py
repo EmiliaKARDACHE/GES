012abfcf23bc4b5405c3ff7b2e059c65cbdcb787
Emilia Kardache <emiliakardache@yahoo.com>
2025-02-09 21:27:54 +0100

    Ajout des images

--- code/etudes.py
+++ code/etudes.py
@@ -1,26 +1,20 @@
+
 import streamlit as st
-import os
 
 def afficher_page_etudes():
     st.title("Nos Études 📚")  
     st.write("Bienvenue sur la page dédiée à nos études. Voici quelques informations utiles.")
     
-    # Dossier où se trouvent les images
-    image_path = os.path.join("Final", "images")     
-
-    # Liste des images et des descriptions
     images = [
-        ("ETUDE ALEX ......", os.path.join(image_path, "image1.png")),
-        ("ETUDE ERIC ......", os.path.join(image_path, "image2.png")),
-        ("ETUDE EMILIA .....", os.path.join(image_path, "image3.png")),
-        ("ETUDE EMILIA .....", os.path.join(image_path, "image4.png")),
-    ]
+    ("ETUDE ALEX ......", "Final/images/image1.png"),
+    ("ETUDE ERIC ......", "Final/images/image2.png"),
+    ("ETUDE EMILIA .....", "Final/images/image3.png"),
+    ("ETUDE EMILIA .....", "Final/images/image4.png"),
+]
     
-    # Affichage des images
     for description, image in images:
         st.write(f"**{description}**")  # Texte en gras
-        st.image(image, use_container_width=True) 
-
+        st.image(image, use_container_width=True)
 
 
 
