37ce4efa9d9bb9dd8fa9e8535ee562a3ddb2b122
Emilia Kardache <emiliakardache@yahoo.com>
2025-02-10 08:56:50 +0100

    Ajout des images

--- code/.ipynb_checkpoints/etudes-checkpoint.py
+++ code/.ipynb_checkpoints/etudes-checkpoint.py
@@ -6,11 +6,11 @@ def afficher_page_etudes():
     st.write("Bienvenue sur la page dédiée à nos études. Voici quelques informations utiles.")
     
     images = [
-        ("ETUDE ALEX ......", "static/images/image1.png"),
-        ("ETUDE ERIC ......", "static/images/image2.png"),
-        ("ETUDE EMILIA .....", "static/images/image3.png"),
-        ("ETUDE EMILIA .....", "static/images/image4.png"),
-    ] 
+        ("ETUDE ALEX ......", "../images/image1.png"),
+        ("ETUDE ERIC ......", "../images/image2.png"),
+        ("ETUDE EMILIA .....", "../images/image3.png"),
+        ("ETUDE EMILIA .....", "../images/image4.png"),  
+    ]
 
     
     for description, image in images:
